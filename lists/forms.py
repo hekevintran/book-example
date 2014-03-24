2a61121659c24f5228d82bc8bf1db75fa2f85ce4
Harry <hjwp2@cantab.net>
2014-03-24 22:53:02 +0000

    switch over to new new_list view. fix missing form save return val

--- lists/forms.py
+++ lists/forms.py
@@ -40,6 +40,7 @@ class NewListForm(ItemForm):
         item.list = list_
         item.text = self.cleaned_data['text']
         item.save()
+        return list_
 
 
 
--- lists/views.py
+++ lists/views.py
@@ -14,16 +14,14 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List()
-        list_.owner = request.user
+        if request.user.is_authenticated():
+            list_.owner = request.user
         list_.save()
         form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'home.html', {"form": form})
 
-
-class NewListForm(object):
-    pass
 
 def new_list2(request):
     form = NewListForm(data=request.POST)
